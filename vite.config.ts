43cd6ea96553822574c145697de4c5c887566c1c
Vikashiu <163726384+Vikashiu@users.noreply.github.com>
2026-01-21 15:09:59 +0530

    Add allowed host to Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: [
+            'blog-e6bi.onrender.com'
+        ]
       },
       plugins: [react()],
       define: {
